26dd91a3a9f4810bdeb5f0668e89618898bd25fc
Space Santa <space-santa@outlook.com>
2024-11-02 08:17:10 +1100

    Init spring boot

---
+++ colours/src/main/java/space/clau/colours/ColoursApplication.java
@@ -0,0 +1,13 @@
+package space.clau.colours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoursApplication.class, args);
+	}
+
+}
