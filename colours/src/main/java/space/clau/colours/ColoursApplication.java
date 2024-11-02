326efaeacb8bea9137b98fc5c9754cb1a7763fff
Space Santa <space-santa@outlook.com>
2024-11-02 11:03:12 +1100

    Only include ColoursApplication when testing

--- colours/src/main/java/space/clau/colours/ColoursApplication.java
+++ colours/src/main/java/space/clau/colours/ColoursApplication.java
@@ -2,8 +2,11 @@ package space.clau.colours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DataLoaderApplication.class))
 public class ColoursApplication {
 
 	public static void main(String[] args) {
