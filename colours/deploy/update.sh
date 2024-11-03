8d991ba987044737c0ce6dab795d9ab2331910a6
Space Santa <space-santa@outlook.com>
2024-11-03 14:55:39 +1100

    Update update

--- colours/deploy/update.sh
+++ colours/deploy/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp PLACEHOLDER.jar /opt/liturgical-colours/bin/
+cp seed_db.sh /opt/liturgical-colours/bin/
+cp start_app.sh /opt/liturgical-colours/bin/
 
 sudo systemctl stop liturgical-colours.service
 sudo systemctl start liturgical-colours.service
