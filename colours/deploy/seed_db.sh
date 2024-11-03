2564ddb7a9218b4bc1924bb8b66efe7b253500e0
Space Santa <space-santa@outlook.com>
2024-11-03 14:45:08 +1100

    Update path

--- colours/deploy/seed_db.sh
+++ colours/deploy/seed_db.sh
@@ -4,4 +4,4 @@
 source /opt/liturgical-colours/db.conf
 
 # Run the application with the prod profile
-java -jar PLACEHOLDER.jar load-data --spring.profiles.active=prod
+java -jar /opt/liturgical-colours/bin/PLACEHOLDER.jar load-data --spring.profiles.active=prod
--- colours/deploy/start_app.sh
+++ colours/deploy/start_app.sh
@@ -4,4 +4,4 @@
 source /opt/liturgical-colours/db.conf
 
 # Run the application with the prod profile
-java -jar PLACEHOLDER.jar --spring.profiles.active=prod
+java -jar /opt/liturgical-colours/bin/PLACEHOLDER.jar --spring.profiles.active=prod
